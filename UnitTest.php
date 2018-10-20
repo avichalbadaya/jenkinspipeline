b3c61ced84e7a8137c7460c31f3e0327f798c65e
Avichal Badaya <avichal.badaya@gmail.com>
2018-10-19 21:38:47 -0400

    Update UnitTest.php

--- UnitTest.php
+++ UnitTest.php
@@ -1,7 +1,7 @@
 <?php
 use PHPUnit\Framework\TestSuite;
 
-class NoAssertTest extends PHPUnit_Framework_TestSuite
+class UnitTest extends PHPUnit_Framework_TestSuite
 {
     function testWithoutAssertions() {
         $x = 5;
